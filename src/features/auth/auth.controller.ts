cd9ef5d321425db8abfa93084f8a55baec7b97a2
David200197 <david.alfonso999999@gmail.com>
2023-12-06 12:23:14 -0500

    add refresh controller

--- src/features/auth/auth.controller.ts
+++ src/features/auth/auth.controller.ts
@@ -1,22 +1,30 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { CommandBus } from '@nestjs/cqrs';
 import { LoginAuthDto } from './dto/login-auth.dto';
 import { LoginAuthResponseDto } from './dto/login-auth-response.dto';
 import { LoginAuthCommand } from './handlers/login-auth/login-auth.command';
 import { SerializerResponse } from '@src/common/lib/response.lib';
+import { RefreshAuthCommand } from './handlers/refresh-auth/refresh-auth.command';
+import { RefreshAuthResponseDto } from './dto/refresh-auth-response.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(
-    private readonly commandBus: CommandBus,
-    private readonly queryBus: QueryBus,
-  ) {}
+  constructor(private readonly commandBus: CommandBus) {}
 
   @Post('login')
   async login(@Body() loginAuthDto: LoginAuthDto) {
     const login: LoginAuthResponseDto = await this.commandBus.execute(
       new LoginAuthCommand(loginAuthDto),
     );
-    return new SerializerResponse('auth was created', { ...login });
+    return new SerializerResponse('login user success', { ...login });
+  }
+
+  @Post('refresh')
+  async refresh() {
+    // TODO: get the authenticate jwt user
+    const refresh: RefreshAuthResponseDto = await this.commandBus.execute(
+      new RefreshAuthCommand({} as any),
+    );
+    return new SerializerResponse('refresh token success', { ...refresh });
   }
 }
