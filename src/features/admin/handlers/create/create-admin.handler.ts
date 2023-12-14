87f6cae5dc40e05d5e18d5cf754e2d735665c844
David200197 <david.alfonso999999@gmail.com>
2023-12-13 23:42:08 -0500

    fix

--- src/features/admin/handlers/create/create-admin.handler.ts
+++ src/features/admin/handlers/create/create-admin.handler.ts
@@ -5,15 +5,24 @@ import { CreateAdminHandlerModel } from './create-admin-handler.model';
 import { Inject } from '@nestjs/common';
 import { ADMIN_REPOSITORY_TOKEN } from '../../providers/admin-repository.provider';
 import { AdminModel } from '../../models/admin.model';
+import { HASH_PASSWORD_SERVICE_TOKEN } from '@src/shared/hash-password/hash-password-service.provider';
+import { HashPasswordServiceModel } from '@src/shared/hash-password/hash-password-helper.service';
 
 @CommandHandler(CreateAdminCommand)
 export class CreateAdminHandler implements CreateAdminHandlerModel {
   constructor(
     @Inject(ADMIN_REPOSITORY_TOKEN)
     private adminRepository: AdminRepositoryModel,
+    @Inject(HASH_PASSWORD_SERVICE_TOKEN)
+    private readonly hashPasswordService: HashPasswordServiceModel,
   ) {}
 
-  execute({ createAdminDto }: CreateAdminCommand): Promise<AdminModel> {
-    return this.adminRepository.create(createAdminDto);
+  async execute({ createAdminDto }: CreateAdminCommand): Promise<AdminModel> {
+    const { password } = createAdminDto;
+    const hashedPassword = await this.hashPasswordService.hash(password);
+    return this.adminRepository.create({
+      ...createAdminDto,
+      password: hashedPassword,
+    });
   }
 }
