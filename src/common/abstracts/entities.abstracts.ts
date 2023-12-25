13a62d46f415cc843ef08508964eb7decec13319
David200197 <david.alfonso999999@gmail.com>
2023-12-25 13:47:58 -0500

    fix

--- src/common/abstracts/entities.abstracts.ts
+++ src/common/abstracts/entities.abstracts.ts
@@ -4,31 +4,105 @@ import { ValueObjectsToProps } from '../interfaces/value-objects-to-props';
 import { isDeeplyEqual } from '../utils/is-deeply-equal';
 import { EntityModel } from './entity.abstract';
 
+/**
+ * Class representing an Entities.
+ * @template T - Array Properties.
+ */
 export interface EntitiesModel<A extends EntityModel<Record<string, unknown>>>
   extends ValueObject<A[]> {
   readonly value: A[];
+  /**
+   * Groups the entities by the provided key
+   * @param getKey - A function that returns the key to group by
+   * @returns An object with the groups as keys and the entities as values
+   */
   groupBy(getKey: (event: A) => number | string): Record<number | string, A[]>;
+  /**
+   * Checks if the entities array is empty
+   * @returns True if the array is empty, false otherwise
+   */
   isEmpty(): boolean;
+  /**
+   * Checks if the entities array is not empty
+   * @returns True if the array is not empty, false otherwise
+   */
   isNotEmpty(): boolean;
+  /**
+   * Maps over the entities array and applies a function to each element
+   * @param method - The method to apply to each element
+   * @returns An array with the mapped values
+   */
   map<T>(method: (value: A, index: number, array: A[]) => T): T[];
+  /**
+   * Asynchronously maps over the entities array and applies a promise-returning function to each element
+   * @param method - The method to apply to each element
+   * @returns A promise that resolves to an array with the mapped values
+   */
   mapAsync<T>(
     method: (value: A, index: number, array: A[]) => Promise<T>,
   ): Promise<T[]>;
+  /**
+   * Asynchronously maps over the entities array and applies a function to each element in parallel
+   * @param method - The method to apply to each element
+   * @returns A promise that resolves to an array of settled promises with the mapped values
+   */
   mapParallel<T>(
     method: (value: A, index: number, array: A[]) => T,
   ): Promise<PromiseSettledResult<Awaited<T>>[]>;
+  /**
+   * Applies a function to each element in the entities array
+   * @param method - The method to apply to each element
+   */
   forEach(method: (value: A, index: number, array: A[]) => void): void;
+  /**
+   * Asynchronously applies a promise-returning function to each element in the entities array
+   * @param method - The method to apply to each element
+   * @returns A promise that resolves when all iterations are completed
+   */
   forEachAsync(
     method: (value: A, index: number, array: A[]) => Promise<void>,
   ): Promise<void>;
+  /**
+   * Asynchronously maps over the entities array and applies a function to each element in parallel
+   * @param method - The method to apply to each element
+   * @returns A promise that resolves to an array of settled promises with the mapped values
+   */
   forEachParallel(
     method: (value: A, index: number, array: A[]) => void,
   ): Promise<PromiseSettledResult<void>[]>;
+  /**
+   * Creates a new Entities instance with the same values as the current one
+   * @returns A new Entities instance with the same values
+   */
   clone(): EntitiesModel<A>;
+  /**
+   * Returns an array with the values of the entities, applying a select options flags
+   * @param options - The select options flags to apply
+   * @returns An array with the selected values
+   */
   select(options: Partial<OptionsFlags<A['value']>>): Record<string, unknown>[];
+  /**
+   * Returns an array with the values of the entities, ignoring properties defined by the ignore options flags
+   * @param options - The ignore options flags to apply
+   * @returns An array with the ignored values
+   */
   ignore(options: Partial<OptionsFlags<A['value']>>): Record<string, unknown>[];
+  /**
+   * Checks if two entities arrays are deeply equal
+   * @param entities - The entities to compare against
+   * @returns True if the arrays are deeply equal, false otherwise
+   */
   isEqual(entities: EntitiesModel<A>): boolean;
+  /**
+   * Adds the entities of another Entities instance to the current one and returns a new Entities instance
+   * @param entities - The entities to add
+   * @returns A new Entities instance with the entities added
+   */
   add(entities: EntitiesModel<A>): EntitiesModel<A>;
+  /**
+   * Converts the entities array to an array of objects
+   * @returns An array of objects with the values of the entities
+   */
   toObject(): ValueObjectsToProps<A['value']>[];
 }
 
@@ -37,9 +111,7 @@ export class Entities<A extends EntityModel<Record<string, unknown>>>
 {
   protected constructor(readonly value: A[]) {}
 
-  groupBy = (
-    getKey: (event: A) => number | string,
-  ): Record<number | string, A[]> => {
+  groupBy(getKey: (event: A) => number | string): Record<number | string, A[]> {
     return this.value.reduce((accumulator, currentValue) => {
       const key = getKey(currentValue);
       if (!key) return { ...accumulator };
@@ -50,7 +122,7 @@ export class Entities<A extends EntityModel<Record<string, unknown>>>
           : new Entities([currentValue]),
       };
     }, {});
-  };
+  }
 
   isEmpty(): boolean {
     return !Boolean(this.value.length);
--- src/common/abstracts/entity.abstract.ts
+++ src/common/abstracts/entity.abstract.ts
@@ -5,19 +5,77 @@ import { ValueObject } from '../interfaces/value-object';
 import { deepClone } from '../utils/deep-clone';
 import { isDeeplyEqual } from '../utils/is-deeply-equal';
 
+/**
+ * Class representing an Entity.
+ * @template T - Properties.
+ */
 export interface EntityModel<T extends Record<string, unknown>>
   extends ValueObject<PropsToValueObjects<T>> {
+  /**
+   * Get the properties of the Entity.
+   * @returns {string[]} - The array of property names.
+   */
   getProperties(): string[];
+  /**
+   * Get the values of the Entity.
+   * @returns {any[]} - The array of property values.
+   */
   getValues(): any[];
+  /**
+   * Get the key-value entries of the Entity.
+   * @returns {[string, any][]} - The array of key-value entries.
+   */
   getEntries(): [string, any][];
+  /**
+   * Map the Entity to a new value using a callback function.
+   * @param {Function} mutate - The callback function to apply to the Entity.
+   * @returns {any} - The mapped value.
+   */
   map<T>(mutate: (value: this) => T): T;
+  /**
+   * Create a shallow clone of the Entity.
+   * @returns {Entity} - The cloned Entity.
+   */
   clone(): this;
+  /**
+   * Perform a method on each property of the Entity.
+   * @param {Function} method - The method to apply to each property.
+   */
   forEachProperty(method: (key: unknown, value: string) => void): void;
+  /**
+   * Select the properties of the Entity based on a set of options.
+   * @param {Partial<OptionsFlags<T>>} options - The options specifying which properties to select.
+   * @returns {Record<string, unknown>} - The selected properties.
+   */
   select(options: Partial<OptionsFlags<T>>): Record<string, unknown>;
+  /**
+   * Ignore the properties of the Entity based on a set of options.
+   * @param {Partial<OptionsFlags<T>>} options - The options specifying which properties to ignore.
+   * @returns {Record<string, unknown>} - The ignored properties.
+   */
   ignore(options: Partial<OptionsFlags<T>>): Record<string, unknown>;
+  /**
+   * Determine if the Entity is equal to another Entity.
+   * @param {Entity} entity - The Entity to compare.
+   * @returns {boolean} - True if the Entities are equal, false otherwise.
+   */
   isEqual(entity: this): boolean;
+  /**
+   * Determine if the Entity is equal to a DeepPartial of T.
+   * @param {DeepPartial<T>} entity - The DeepPartial to compare.
+   * @returns {boolean} - True if the Entity is equal to the DeepPartial, false otherwise.
+   */
   isSelfEqual(entity: DeepPartial<T>): boolean;
+  /**
+   * Convert the Entity to an object.
+   * @returns {T} - The Entity as an object.
+   */
   toObject(): T;
+  /**
+   * Get the value of a specific property.
+   * @param {string} key - The key of the property.
+   * @returns {any} - The value of the property.
+   */
   get<P extends keyof T>(key: P): T[P];
 }
 
